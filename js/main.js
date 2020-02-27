726f7003fb8bb5c5d15d6fdb5ac62edc51262c20
Casey The Great <casey@caseypitman.com>
2020-02-27 13:56:00 -0600

    Basic HTML structure. No C SS. Built Array

--- js/main.js
+++ js/main.js
@@ -1,2 +1,53 @@
 //main js
 
+//Array of information about family members
+const familyArr=[
+    {name: "Casey",
+     nickname: "Dad",
+     position: "Father",
+     age: "42",
+     favoriteFood: "Mint Chocolate Chip Ice Cream",
+     favoriteBook: "The Stand by Stephen King",
+     favoriteMovie: "The Shawshank Redemption",
+     favoriteShow: "LOST",
+     favoriteSong: "'I Believe' by R.E.M.",
+     favoriteColor: "Green",
+     favoritePet: "Ringo"
+    },
+    {name: "Traci",
+     nickname: "Latte",
+     position: "Mother",
+     age: "35",
+     favoriteFood:
+     favoriteBook:
+     favoriteMovie:
+     favoriteShow:
+     favoriteSong:
+     favoriteColor:
+     favoritePet:
+    },
+    {name: "Charlie",
+     nickname: "Two-Butt Chuck | Charliepants",
+     position: "Child (boy)",
+     age: "8",
+     favoriteFood:
+     favoriteBook:
+     favoriteMovie:
+     favoriteShow:
+     favoriteSong:
+     favoriteColor:
+     favoritePet:
+    },
+    {name: "Lily Beth",
+     nickname: "Wiggly Beth | Nugget",
+     position: "Child (girl)",
+     age: "5",
+     favoriteFood:
+     favoriteBook:
+     favoriteMovie:
+     favoriteShow:
+     favoriteSong:
+     favoriteColor:
+     favoritePet:
+    }
+];
